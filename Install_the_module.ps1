3d2a61e3fb1fa3d677311772c81fc097f80739be
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-02-06 17:35:09 +0100

    Add files via upload

---
+++ Install_the_module.ps1
@@ -0,0 +1,22 @@
+﻿Set-Location c:\
+Clear-Host
+
+#Install the Az Module
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Verify the WVD Moduel is Installed
+Get-InstalledModule -Name Az.Desk*
+
+#Install the WVD module Only
+Install-Module -Name Az.DesktopVirtualization
+
+#Update the module
+Update-Module Az.DesktopVirtualization
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzContext
+Get-AzSubscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
