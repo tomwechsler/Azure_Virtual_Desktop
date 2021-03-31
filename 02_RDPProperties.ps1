e4206175633dfda15feadb3f90bb3cf60506cc1a
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-03-31 10:49:34 +0200

    Update 02_RDPProperties.ps1

--- 02_RDPProperties.ps1
+++ 02_RDPProperties.ps1
@@ -4,7 +4,7 @@ Clear-Host
 #Install the Az Module
 Install-Module -Name Az -Force -AllowClobber -Verbose
 
-#Verify the WVD Moduel is Installed
+#Verify the WVD Modul is Installed
 Get-InstalledModule -Name Az.Desk*
 
 #Install the WVD module Only
@@ -22,15 +22,15 @@ Get-AzSubscription
 Get-AzSubscription -SubscriptionName "Nutzungsbasierte Bezahlung" | Select-AzSubscription
 
 #View the current RDP Settings
-Get-AzWvdHostPool -ResourceGroupName <WVD_ResourceGroup> -Name <WVD_HostPool> | Format-list Name, CustomRdpProperty
+Get-AzWvdHostPool -ResourceGroupName elme-rg -Name elme-hostpool | Format-list Name, CustomRdpProperty
 
 #Remove existing settings
-Update-AzWvdHostPool -ResourceGroupName <WVD_ResourceGroup> -Name <WVD_HostPool> -CustomRdpProperty ""  
+Update-AzWvdHostPool -ResourceGroupName elme-rg -Name elme-hostpool -CustomRdpProperty ""  
 
 #Add an RDP Property (With this method the values are always overwritten)
-Update-AzWvdHostPool -ResourceGroupName <WVD_ResourceGroup> -Name <WVD_HostPool> -CustomRdpProperty redirectclipboard:i:0
-Update-AzWvdHostPool -ResourceGroupName <WVD_ResourceGroup> -Name <WVD_HostPool> -CustomRdpProperty redirectprinters:i:0
+Update-AzWvdHostPool -ResourceGroupName elme-rg -Name elme-hostpool -CustomRdpProperty redirectclipboard:i:0
+Update-AzWvdHostPool -ResourceGroupName elme-rg -Name elme-hostpool -CustomRdpProperty redirectprinters:i:0
 
-# Add multiple RDP Properties
+#Add multiple RDP Properties
 $properties = "redirectclipboard:i:0;redirectprinters:i:0;drivestoredirect:s:"
-Update-AzWvdHostPool -ResourceGroupName <WVD_ResourceGroup> -Name <WVD_HostPool> -CustomRdpProperty $properties
+Update-AzWvdHostPool -ResourceGroupName elme-rg -Name elme-hostpool -CustomRdpProperty $properties
